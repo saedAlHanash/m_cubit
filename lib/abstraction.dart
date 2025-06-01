2bf88fef7afd941dceeccd7f2e441c91e5a659fd
saedAlHanash <SaedAlHanash@gmail.com>
2025-06-01 12:49:17 +0300

    fix name files

--- lib/abstraction.dart
+++ lib/abstraction.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
