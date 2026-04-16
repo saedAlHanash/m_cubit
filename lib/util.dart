7ada2133c42840fad1b8cdd98f405fbdf5c4c5ec
saedAlHanash <SaedAlHanash@gmail.com>
2026-04-16 12:59:07 +0300

    update dependencies and add string extensions for enhanced functionality

--- lib/util.dart
+++ lib/util.dart
@@ -1,4 +1,334 @@
 import 'abstraction.dart';
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+import 'package:intl/intl.dart';
+import 'package:m_cubit/util.dart';
+
+import 'abstraction.dart';
+
+extension SplitByLength on String {
+  String maxLength(int l) {
+    if (length > l) return substring(0, l);
+    return this;
+  }
+
+  String get getKey {
+    var digest2 = hashCode.toString();
+    return digest2.maxLength(10);
+  }
+
+  List<String> splitByLength1(int length, {bool ignoreEmpty = false}) {
+    List<String> pieces = [];
+
+    for (int i = 0; i < this.length; i += length) {
+      int offset = i + length;
+      String piece = substring(i, offset >= this.length ? this.length : offset);
+
+      if (ignoreEmpty) {
+        piece = piece.replaceAll(RegExp(r'\s+'), '');
+      }
+
+      pieces.add(piece);
+    }
+    return pieces;
+  }
+
+  String get logLongMessage {
+    var r = [];
+    var res = '';
+    if (length > 800) {
+      r = splitByLength1(800);
+      for (var e in r) {
+        res += '$e\n';
+      }
+    } else {
+      res = this;
+    }
+    return res;
+  }
+
+  bool get canSendToSearch {
+    if (isEmpty) false;
+
+    return split(' ').last.length > 2;
+  }
+
+  int get numberOnly {
+    final regex = RegExp(r'\d+');
+
+    final numbers = regex.allMatches(this).map((match) => match.group(0)).join();
+
+    try {
+      return int.parse(numbers);
+    } on Exception {
+      return 0;
+    }
+  }
+
+  bool get isZero => (num.tryParse(this) ?? 0) == 0;
+
+  String get removeDuplicates => split(' ').toSet().join(' ');
+
+  num get tryParseOrZero => num.tryParse(this) ?? 0.0;
+
+  num tryParseOr(num n) => num.tryParse(this) ?? n;
+
+  int get tryParseOrZeroInt => int.tryParse(this) ?? 0;
+
+  num? get tryParseOrNull => num.tryParse(this);
+
+  int? get tryParseOrNullInt => int.tryParse(this);
+
+  bool get tryParseBoolOrFalse => this == '1' || toLowerCase() == 'true';
+}
+
+extension StringHelper on String? {
+  String get fixUrl {
+    if (this == null) return '';
+    if ((this ?? '').startsWith('http')) return this ?? '';
+    final String link = "http://e-learning.testbandtech.com/storage/images/$this";
+    return link;
+  }
+
+  String? get fixPhone {
+    if (this == null) return null;
+    if ((this ?? '').startsWith('+964')) return this ?? '';
+    final p = this!
+        .replaceAll('٠', '0')
+        .replaceAll('١', '1')
+        .replaceAll('٢', '2')
+        .replaceAll('٣', '3')
+        .replaceAll('٤', '4')
+        .replaceAll('٥', '5')
+        .replaceAll('٦', '6')
+        .replaceAll('٧', '7')
+        .replaceAll('٨', '8')
+        .replaceAll('٩', '9');
+
+    return '+964$p'.replaceAll('+9640', '+964');
+  }
+
+  String? get getVideoId => this?.split('/').lastOrNull;
+
+  bool get isBlank {
+    if (this == null) return true;
+    if (this == 'null') return true;
+    return this!.trim().isEmpty;
+  }
+}
+
+extension MaxIntNulable on num? {
+  bool get isBlankNumber {
+    if (this == null) return true;
+    return (this!) <= 0;
+  }
+}
+
+extension HelperJson on Map<String, dynamic> {
+  num getAsNum(String key) {
+    if (this[key] == null) return -1;
+    return num.tryParse((this[key]).toString()) ?? -1;
+  }
+}
+
+extension FormatDuration on Duration {
+  String get format {
+    var includeDays = false;
+
+    final h = inHours.remainder(24);
+    final m = inMinutes.remainder(60);
+    final s = inSeconds.remainder(60);
+
+    final buffer = StringBuffer();
+
+    // if (includeDays && d > 0) buffer.write('${d.toString().padLeft(2, '0')}:');
+    if (includeDays || h > 0) buffer.write('${h.toString().padLeft(2, '0')}:');
+
+    buffer
+      ..write('${m.toString().padLeft(2, '0')}:')
+      ..write(s.toString().padLeft(2, '0'));
+
+    return buffer.toString();
+  }
+}
+
+extension ApiStatusCode on int {
+  bool get success => (this >= 200 && this <= 210);
+
+  DateTime get fromMilliDateFixed => DateTime.fromMillisecondsSinceEpoch(this).toUtc().fixTimeZone;
+
+  int get countDiv2 => (this ~/ 2 < this / 2) ? this ~/ 2 + 1 : this ~/ 2;
+}
+
+extension TextEditingControllerHelper on TextEditingController {
+  void clear() {
+    if (text.isNotEmpty) text = '';
+  }
+}
+
+extension DateUtcHelper on DateTime {
+  int get hashDate => (day * 61) + (month * 83) + (year * 23);
+
+  String get formatDateTime1 {
+    return DateFormat('dd MMM yyyy  h:mm a', 'en').format(this);
+  }
+
+  DateTime get getUtc => DateTime.utc(year, month, day);
+
+  /// Check if the date is today
+  bool get isToday {
+    final now = DateTime.now();
+    return now.year == year && now.month == month && now.day == day;
+  }
+
+  /// Check if the date is tomorrow
+  bool get isTomorrow {
+    final tomorrow = DateTime.now().add(Duration(days: 1));
+    return tomorrow.year == year && tomorrow.month == month && tomorrow.day == day;
+  }
+
+  /// Check if the date is yesterday
+  bool get isYesterday {
+    final yesterday = DateTime.now().subtract(Duration(days: 1));
+    return yesterday.year == year && yesterday.month == month && yesterday.day == day;
+  }
+
+  String get formatDate => DateFormat('yyyy/MM/dd', 'en').format(this);
+
+  String get formatDateMonthName => '$monthName $day';
+
+  String get formatDateMD => DateFormat('dd/MM', 'en').format(this);
+
+  String get formatDateDY => DateFormat('yyyy/MM', 'en').format(this);
+
+  String get formatDateDYMName => '';
+
+  String get formatDateD => DateFormat('dd', 'en').format(this);
+
+  String get formatDateToRequest => DateFormat('yyyy-MM-dd', 'en').format(this);
+
+  String get formatTime => DateFormat('hh:mm a', 'en').format(this);
+
+  String get formatTime24 => DateFormat('hh:mm', 'en').format(this);
+
+  String get dayName => DateFormat('EEEE').format(this);
+
+  String get monthName => DateFormat('MMMM').format(this);
+
+  String get formatDateTime => '$formatDate - $formatTime';
+
+  String get formatDateTime24 => '$formatDate - $formatTime24';
+
+  String get formatDateTimeVertical => '$formatDate\n$formatTime';
+
+  DateTime addFromNow({int? year, int? month, int? day, int? hour, int? minute, int? second}) {
+    return DateTime(
+      this.year + (year ?? 0),
+      this.month + (month ?? 0),
+      this.day + (day ?? 0),
+      this.hour + (hour ?? 0),
+      this.minute + (minute ?? 0),
+      this.second + (second ?? 0),
+    );
+  }
+
+  FormatDateTime getFormat({DateTime? serverDate}) {
+    final difference = this.difference(serverDate ?? DateTime.now());
+
+    final months = difference.inDays.abs() ~/ 30;
+    final days = difference.inDays.abs() % 360;
+    final hours = difference.inHours.abs() % 24;
+    final minutes = difference.inMinutes.abs() % 60;
+    final seconds = difference.inSeconds.abs() % 60;
+    return FormatDateTime(
+      months: months,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    );
+  }
+
+  int get getWeekNumber {
+    final DateTime firstJan = DateTime(year, 1, 1);
+    // final int daysInYear = DateTime(year + 1, 1, 1).difference(firstJan).inDays;
+    final int weekNumber = (difference(firstJan).inDays ~/ 7) + 1;
+    // If the date is after the first Monday of the year, then it is in the current week.
+    if (weekday >= 1) {
+      return weekNumber;
+    }
+    // Otherwise, it is in the previous week.
+    return weekNumber - 1;
+  }
+
+  DateTime get fixTimeZone => add(DateTime.now().timeZoneOffset);
+
+  List<DateTime> getDateTimesBetween({
+    required DateTime end,
+    required Duration period,
+  }) {
+    var dateTimes = <DateTime>[];
+    var current = add(period);
+    while (current.isBefore(end)) {
+      if (dateTimes.length > 24) {
+        break;
+      }
+      dateTimes.add(current);
+      current = current.add(period);
+    }
+    return dateTimes;
+  }
+}
+
+extension FirstItem<E> on Iterable<E> {
+  E? get firstItem => isEmpty ? null : first;
+}
+
+class FormatDateTime {
+  final int months;
+  final int days;
+  final int hours;
+  final int minutes;
+  final int seconds;
+
+  const FormatDateTime({
+    required this.months,
+    required this.days,
+    required this.hours,
+    required this.minutes,
+    required this.seconds,
+  });
+
+  @override
+  String toString() {
+    return '$months\n'
+        '$days\n'
+        '$hours\n'
+        '$minutes\n'
+        '$seconds\n';
+  }
+}
+
+extension NeedUpdateEnumH on NeedUpdateEnum {
+  bool get loading => this == NeedUpdateEnum.withLoading;
+
+  bool get haveData => this == NeedUpdateEnum.no || this == NeedUpdateEnum.noLoading;
+
+  CubitStatuses get getState {
+    switch (this) {
+      case NeedUpdateEnum.no:
+        return CubitStatuses.done;
+      case NeedUpdateEnum.withLoading:
+        return CubitStatuses.loading;
+      case NeedUpdateEnum.noLoading:
+        return CubitStatuses.noLoading;
+    }
+  }
+}
 
 enum FilterOrderBy {
   desc,
@@ -47,40 +377,3 @@ enum FilterOperation {
 }
 
 enum NeedUpdateEnum { no, withLoading, noLoading }
-
-extension McubitStringH on String {
-  String maxLength(int l) {
-    if (length > l) return substring(0, l);
-    return this;
-  }
-
-  String get getKey {
-    var digest2 = hashCode.toString();
-    return digest2.maxLength(10);
-  }
-}
-
-extension StringHelper on String? {
-  bool get isBlank {
-    if (this == null) return true;
-    if (this == 'null') return true;
-    return this!.trim().isEmpty;
-  }
-}
-
-extension NeedUpdateEnumH on NeedUpdateEnum {
-  bool get loading => this == NeedUpdateEnum.withLoading;
-
-  bool get haveData => this == NeedUpdateEnum.no || this == NeedUpdateEnum.noLoading;
-
-  CubitStatuses get getState {
-    switch (this) {
-      case NeedUpdateEnum.no:
-        return CubitStatuses.done;
-      case NeedUpdateEnum.withLoading:
-        return CubitStatuses.loading;
-      case NeedUpdateEnum.noLoading:
-        return CubitStatuses.noLoading;
-    }
-  }
-}
