87123cc5613a8eddb2bd3323850c725934ddb5b5
saedAlHanash <SaedAlHanash@gmail.com>
2026-08-23 15:46:53 +0300

    add error handling in caching service and implement getOrNull extension for List

--- lib/m_cubit.dart
+++ lib/m_cubit.dart
@@ -3,5 +3,4 @@ library m_cubit;
 export 'caching_service/caching_service.dart';
 export 'abstraction.dart';
 export 'command.dart';
-export 'caching_service/cacheable_interface.dart';
 export 'util.dart';
